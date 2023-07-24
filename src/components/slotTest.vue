e5c93e1a7e9b7817975bad498c7e7678d9974cb5
changxianglin <281532612@qq.com>
2023-07-25 00:04:25 +0800

    v-slot: instan of #

--- src/components/slotTest.vue
+++ src/components/slotTest.vue
@@ -4,6 +4,13 @@
     <div>
       <pre>大江东去浪淘尽，千古风流人物。</pre>
     </div>
+    <!--  -->
+    <template #a>
+      <div>具名插槽位置结构a</div>
+    </template>
+    <template v-slot:b>
+      <div>具名插槽位置结构b</div>
+    </template>
   </TestSlot>
 </template>
 
