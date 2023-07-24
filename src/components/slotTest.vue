94b4e9cec8b55c6224464429059e7f86b15ca261
changxianglin <281532612@qq.com>
2023-07-25 00:22:39 +0800

    scope slot

--- src/components/slotTest.vue
+++ src/components/slotTest.vue
@@ -1,5 +1,10 @@
 <template>
   <p>slot</p>
+  <Test1 :todos="todos">
+    <template v-slot="{$row, $index}">
+      <span :style="{color: $row.done ? 'green': 'red'}">{{ $row.title }} -- {{ $index }}</span>
+    </template>
+  </Test1>
   <TestSlot>
     <div>
       <pre>大江东去浪淘尽，千古风流人物。</pre>
@@ -16,4 +21,15 @@
 
 <script lang="ts" setup>
 import TestSlot from './testSlot.vue'
+import Test1 from './test3.vue'
+
+import { ref } from 'vue'
+
+let todos = ref([
+  {id: 1, title: '吃饭', done: true},
+  {id: 2, title: '睡觉', done: false},
+  {id: 3, title: '打豆豆', done: true},
+  {id: 4, title: '打代码', done: false},
+])
+
 </script>
---
+++ src/components/test3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <h1>作用域插槽</h1>
+    <ul>
+      <li v-for="(item, index) in todos" :key="item.id">
+        <slot :$row="item" :$index="index"></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps(['todos'])
+</script>
+
+<style scoped>
+  .box {
+    width: 100vw;
+    height: 400px;
+    background-color: skyblue;
+  }
+</style>
